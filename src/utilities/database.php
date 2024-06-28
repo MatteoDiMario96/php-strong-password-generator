0abac8c0f894e214897e0338f2767699c5b19a12
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-28 16:03:30 +0200

    Creazione dell'array messo in database.php

---
+++ src/utilities/database.php
@@ -0,0 +1,35 @@
+<?php 
+
+$users = [
+    [
+        'userName' => 'Matti000',
+        'passWord' => 'password0'
+    ],[
+        'userName' => 'Matti001',
+        'passWord' => 'password1'
+    ],[
+        'userName' => 'Matti002',
+        'passWord' => 'password2'
+    ],[
+        'userName' => 'Matti003',
+        'passWord' => 'password3'
+    ],[
+        'userName' => 'Matti004',
+        'passWord' => 'password4'
+    ],[
+        'userName' => 'Matti005',
+        'passWord' => 'password5'
+    ],[
+        'userName' => 'Matti006',
+        'passWord' => 'password6'
+    ],[
+        'userName' => 'Matti007',
+        'passWord' => 'password7'
+    ],[
+        'userName' => 'Matti008',
+        'passWord' => 'password8'
+    ],[
+        'userName' => '1234',
+        'passWord' => '1234'
+    ],
+];
